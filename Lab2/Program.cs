e773a5058e2b5f2c2a93f2437d194985606b44c6
hermosaboricua <gizmo1239@aol.com>
2026-07-13 01:38:12 +0000

    part 4

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -77,9 +77,75 @@ class Program
         {
 
             Console.WriteLine("Senior ticket: $6");
+{
+
+        // Ask the user to enter a day number
+
+        Console.Write("Enter a day of the week (1-7): ");
+
+        int day = int.Parse(Console.ReadLine()!);
+
+        // Display the corresponding day
+
+        switch (day)
+
+        {
+
+            case 1:
+
+                Console.WriteLine("Monday");
+
+                break;
+
+            case 2:
+
+                Console.WriteLine("Tuesday");
+
+                break;
+
+            case 3:
+
+                Console.WriteLine("Wednesday");
+
+                break;
+
+            case 4:
+
+                Console.WriteLine("Thursday");
+
+                break;
+
+            case 5:
+
+                Console.WriteLine("Friday");
+
+                break;
+
+            case 6:
+
+                Console.WriteLine("Saturday");
+
+                break;
+
+            case 7:
+
+                Console.WriteLine("Sunday");
+
+                break;
+
+            default:
+
+                Console.WriteLine("Invalid day!");
+
+                break;
 
         }
 
     }
 
 }
+        }
+
+    }
+
+
--- Lab2/obj/Debug/net10.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net10.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aea9fd91037882085ca912722d6026927140aa08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dff1444c5d76748501549953862c9c704a782b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
