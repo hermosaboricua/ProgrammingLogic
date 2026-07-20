320c1439ebf3b649e80f06e444bcad738a19fc53
hermosaboricua <gizmo1239@aol.com>
2026-07-20 02:23:11 +0000

    problem 2

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -2,21 +2,23 @@
 
 class Program
 {
-
     static void Main(string[] args)
-
     {
-
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Problem 1:");
 
         for (int i = 1; i <= 10; i++)
-
         {
-
             Console.WriteLine(i);
-
         }
 
-    }
+        Console.WriteLine("\nProblem 2:");
 
+        for (int i = 1; i <= 20; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
 }
